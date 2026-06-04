6731342b9555e62277365eefefdcfeb370df02f1
a3moon2000 <a3moon2000@gmail.com>
2026-06-04 21:40:38 +1000

    Add Farsi subtitle finder

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Streamr service worker - app shell cache for installability + offline shell */
-var CACHE = 'streamr-v15';
+var CACHE = 'streamr-v16';
 var SHELL = ['./', './index.html', './icon.svg', './manifest.json'];
 
 self.addEventListener('install', function(e) {
