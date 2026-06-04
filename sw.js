24547857a34cbcf97b53893d9fcf121484962429
a3moon2000 <a3moon2000@gmail.com>
2026-06-04 23:08:28 +1000

    Add see all for mind bending row

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Streamr service worker - app shell cache for installability + offline shell */
-var CACHE = 'streamr-v21';
+var CACHE = 'streamr-v22';
 var SHELL = ['./', './index.html', './icon.svg', './manifest.json'];
 
 self.addEventListener('install', function(e) {
