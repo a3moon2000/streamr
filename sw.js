d9bd4bea268a1073cf126d47cb4c936f10fc2866
a3moon2000 <a3moon2000@gmail.com>
2026-06-04 20:46:13 +1000

    Use Farsi subtitle player by default

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Streamr service worker - app shell cache for installability + offline shell */
-var CACHE = 'streamr-v11';
+var CACHE = 'streamr-v12';
 var SHELL = ['./', './index.html', './icon.svg', './manifest.json'];
 
 self.addEventListener('install', function(e) {
