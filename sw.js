aad69615506739608a441fe56d8f162e7150fec5
a3moon2000 <a3moon2000@gmail.com>
2026-06-04 13:16:27 +1000

    Perf: lazy-load rows on scroll, smaller hero/poster images, async decode

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Streamr service worker - app shell cache for installability + offline shell */
-var CACHE = 'streamr-v2';
+var CACHE = 'streamr-v3';
 var SHELL = ['./', './index.html', './icon.svg', './manifest.json'];
 
 self.addEventListener('install', function(e) {
