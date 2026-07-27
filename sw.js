cac2dacee77950eef38c4bb0f7ef0f1808c86ad0
a3moon2000 <a3moon2000@users.noreply.github.com>
2026-07-27 21:00:23 +1000

    feat: add browse filters and fix catalog validation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Streamr service worker - app shell cache for installability + offline shell */
-var CACHE = 'streamr-v82';
+var CACHE = 'streamr-v83';
 var SHELL = ['./', './index.html', './icon.svg', './manifest.json',
              './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
